35a124044f91974435038a81d3915cff96724ca0
godnames <niranjan.hospitalityminds@gmail.com>
2023-10-23 17:43:22 +0530

    commit

--- mail.php
+++ mail.php
@@ -23,7 +23,7 @@
         // FIXME: Update this to your desired email address.
         // $recipient = "tunatheme@gmail.com";
         
-        $recipient = "tunatheme@gmail.com";
+        $recipient = "dirghayuagriresort@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
